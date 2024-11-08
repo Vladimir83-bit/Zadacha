5a8bebbbf5c6d7a6f37ec488d950317ed082b352
Vladimir83-bit <PopovVkap@yandex.ru>
2024-11-08 17:12:14 +0000

    Сделал третье задание

--- task3.py
+++ task3.py
@@ -1,7 +1,10 @@
-number = int(input("Введите число \n"))
-if number > 0:
-    print("Число больше нуля.")
-elif number == 0:
-    print("Число равно нулю.")
-else:
-    print("Число меньше нуля.")
+try:
+    number = int(input("Введите число \n"))
+    if number > 0:
+        print("Число больше нуля.")
+    elif number == 0:
+        print("Число равно нулю.")
+    else:
+        print("Число меньше нуля.")
+except ValueError:
+    print("Это не число!")
