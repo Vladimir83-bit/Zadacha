afb44891f05c13daaa99b37f414d2af2d69268cc
Vladimir83-bit <PopovVkap@yandex.ru>
2024-11-08 17:28:42 +0000

    Сделал пятое задание

---
+++ task5.py
@@ -0,0 +1,11 @@
+number = int(input("Введите число от 1 до 100:"))
+if number < 1 and number > 100:
+    prin("Число не в диапазоне!")
+elif % 3 == 0 and number % 5 == 0:
+    print("Fizz Buzz")
+elif number % 5 == 0:
+    print("Buzz")
+elif number % 3 == 0:
+    print("Fizz")
+else:
+    print(number)
