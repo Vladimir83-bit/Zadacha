7a5dee8b74c9220a01392aaa375796b2d4d4223f
Vladimir83-bit <PopovVkap@yandex.ru>
2024-12-13 18:13:02 +0000

    done t2-17

---
+++ lesson8/t2-17.py
@@ -0,0 +1,7 @@
+number = int(input("Enter number please : "))
+if 100 <= number < 1000:
+    str_number = str(number)
+    swapped_number = int(str_number[0] + str_number[2] + str_number[1])
+    print("Ответ: ", swapped_number)
+else:
+    print("Это не 3-х значное число!")
